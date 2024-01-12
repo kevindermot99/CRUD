0c5d48d35b249718cfffd30da227f51d8ca535d7
kevindermot99 <mbonimpayekevin@gmail.com>
2024-01-12 06:26:25 +0200

    First commit

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import CreateUser from './CreateUser'
+import UpdateUser from './UpdateUser'
+import Users from './Users'
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Users />}></Route>
+          <Route path='/create' element={<CreateUser />}></Route>
+          <Route path='/update' element={<UpdateUser />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
