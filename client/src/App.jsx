1d12cae68c337163b8e43282aca7e88245401892
kevindermot99 <mbonimpayekevin@gmail.com>
2024-01-13 06:02:08 +0200

    Project Done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,48 +5,12 @@ import CreateUser from './CreateUser'
 import UpdateUser from './UpdateUser'
 import Users from './Users'
 
-// icons
-import { TbSunMoon } from "react-icons/tb";
-import { BsLayoutSidebarInset } from "react-icons/bs";
 
 function App() {
-
-  const toggleMenu = () => {
-    const menu = document.querySelector('.side-menu');
-    menu.classList.toggle("hide")
-  }
 
 
   return (
     <>
-      <div className='side-menu'>
-
-        {/* single channel */}
-        <div className='channel nodot'>
-          <div className='channel-pp'>
-          <span className='channel-name'>Switch Theme</span>
-            <TbSunMoon />
-          </div>
-        </div>
-
-        {/* single channel */}
-        <div className='channel'>
-          {/* <div className='channel-pp'>
-            <IoMdAdd />
-          </div> */}
-        </div>
-      </div>
-
-      <div className='always-on-top'>
-        {/* single channel */}
-        <div className='channel nodot'>
-          <div className='channel-pp' onClick={toggleMenu}>
-        <span className='channel-name'>Toggle Menu</span>
-            <BsLayoutSidebarInset />
-          </div>
-        </div>
-      </div>
-
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Users />}></Route>
